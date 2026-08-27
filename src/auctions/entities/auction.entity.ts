3832daaf73fc6a2c7595f41e8ec75f2f5b94c6e1
Simon Nancekievill <simon.nancekievill@gmail.com>
2026-08-27 12:23:46 +0200

    auction entity finished

--- src/auctions/entities/auction.entity.ts
+++ src/auctions/entities/auction.entity.ts
@@ -1,7 +1,33 @@
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity('auctions')
 export class Auction {
   @PrimaryGeneratedColumn('uuid')
   id!: string;
+
+  @Column()
+  title!: string;
+
+  @Column()
+  description!: string;
+
+  @Column()
+  seller!: string;
+
+  @Column()
+  startingPrice!: number;
+
+  @Column({ default: 0 })
+  currentPrice!: number;
+
+  @Column({ type: 'datetime' })
+  endDate!: string;
+
+  @CreateDateColumn()
+  createdAt!: Date;
 }
