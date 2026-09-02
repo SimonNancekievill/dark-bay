cb39151025c3934efb6ed45cfe6950805e8c570d
Maria Traut <mia.gotcha@proton.me>
2026-09-02 15:08:29 +0200

    implement basic swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -19,6 +20,19 @@ async function bootstrap() {
       excludeExtraneousValues: true,
     }),
   );
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Dark Bay API')
+    .setDescription('Auctions, offers and users')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
+  console.log(`http://localhost:${port}`);
 }
 bootstrap();
